17c3d05fdf4d6a93e162bf4a2f817b6cd7ae1224
David Paschich <dpassage@balveda.com>
2013-12-27 17:29:23 -0800

    Initial Commit

---
+++ FunctionalReactivePixels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FunctionalReactivePixels
+//
+//  Created by David Paschich on 12/27/13.
+//  Copyright (c) 2013 David Paschich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRPAppDelegate class]));
+    }
+}
